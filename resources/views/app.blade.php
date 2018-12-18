1e0dfcbcbf81108dcc8c28badfe76d1386880889
luigivivian <luigivivian@hotmail.com>
2018-12-18 07:55:42 -0200

    V1

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,9 +20,7 @@
 
          <div class="collapse navbar-collapse" id="navbarSupportedContent">
              <ul class="navbar-nav mr-auto">
-                 <li class="nav-item active">
-                     <a class="nav-link" href="#">Inicio </a>
-                 </li>
+
                  <li class="nav-item">
                      <a class="nav-link" href="{{route('estados')}}">ESTADOS</a>
                  </li>
